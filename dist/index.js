96819e934f284feabceb0628544f3857c33618f8
marzan <hassanmarzan17@gmail.com>
2024-03-13 14:54:38 +0600

    inheritance

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,52 @@
 "use strict";
+// class User {
+//     email: string
+//     name: string
+//     private city: string = ''
+//     readonly id: number = 1234
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+// const mahir = new User('m@m.com', 'mahir')
+// console.log(mahir.name);
+// // console.log(mahir.city);
+// // mahir.city = 'ctg'
+// // console.log(mahir.city);
 class User {
-    constructor(email, name) {
-        this.city = '';
-        this.id = 1234;
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this._courseCount = 1; //private variables are only accessible inside the class
+        this.id = 1234;
+    }
+    deleteToken() {
+        console.log(`token deleted`);
+    }
+    // getter method
+    get getAppleEmailobject() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    // setter method
+    // setter method can not return anything so it will not have any return type
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNumber;
     }
 }
-const mahir = new User('m@m.com', 'mahir');
-console.log(mahir.name);
-console.log(mahir.city);
-mahir.city = 'ctg';
-console.log(mahir.city);
+//inheritance
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+}
+const mahir = new User('m@m.com', 'mahir', '123');
+// mahir.deleteToken() //can not access it as it is a private token
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,59 @@
+// class User {
+//     email: string
+//     name: string
+//     private city: string = ''
+//     readonly id: number = 1234
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
+// const mahir = new User('m@m.com', 'mahir')
+// console.log(mahir.name);
+// // console.log(mahir.city);
+// // mahir.city = 'ctg'
+// // console.log(mahir.city);
+
 class User {
-    email: string
-    name: string
-    city: string = ''
+    private _courseCount: number = 1 //private variables are only accessible inside the class
     readonly id: number = 1234
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name
+    constructor(
+        public email: string,
+        public name: string,
+        private userId: string) {
+    }
+
+    private deleteToken(){
+        console.log(`token deleted`);
+    }
+
+    // getter method
+    get getAppleEmailobject(): string {
+        return `apple ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    // setter method
+    // setter method can not return anything so it will not have any return type
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error('Course count should be more than 1')
+        }
+        this._courseCount = courseNumber
+    
     }
 }
 
-const mahir = new User('m@m.com', 'mahir')
-console.log(mahir.name);
-console.log(mahir.city);
-mahir.city = 'ctg'
-console.log(mahir.city);
+//inheritance
+class SubUser extends User{
+    isFamily : boolean = true
+
+
+}
+
+const mahir = new User('m@m.com', 'mahir', '123')
+// mahir.deleteToken() //can not access it as it is a private token
